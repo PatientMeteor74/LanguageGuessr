7c85fdb61757d79aac704a3c023af5731f38b0dd
John Platkowski <johnjplatkowski@gmail.com>
2026-01-25 17:01:33 -0600

    Update GameScene.jsx

--- src/components/GameScene.jsx
+++ src/components/GameScene.jsx
@@ -218,7 +218,6 @@ function GameScene({ navigateToScene, score, setScore, guess, setGuess, language
         {
             const newWordNumber = wordNumber + 1
             
-            // Safety check: don't advance beyond total words
             if (newWordNumber > totalWords) 
             {
                 console.log('Already at max words, should be at end screen')
